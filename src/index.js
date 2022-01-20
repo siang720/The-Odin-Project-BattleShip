89b20d391249ad214d3b95fbc0cf1ce53571d13c
siang720 <siang720@gmail.com>
2022-01-20 14:16:48 +1000

    project basic settings

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+const content = document.getElementById("content");
+
+const title = document.createElement("h1");
+title.innerText = "Let's Play Battleship";
+
+content.appendChild(title);
